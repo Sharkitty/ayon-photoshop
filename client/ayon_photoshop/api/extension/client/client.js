701a7e658d67528173215a47d511a3289cb79af3
Sharkitty <hayleylalicorne@gmail.com>
2024-12-26 17:37:00 +0100

    linting and logic fixes

--- client/ayon_photoshop/api/extension/client/client.js
+++ client/ayon_photoshop/api/extension/client/client.js
@@ -2,23 +2,23 @@
     // do that)
     // consumes RPC calls from server (OpenPype) calls ./host/index.jsx and
     // returns values back (in json format)
-    
+
     var logReturn = async function(result){ log.warn('Result: ' + result);};
-    
+
     var csInterface = new CSInterface();
-    
+
     log.warn("script start");
 
     WSRPC.DEBUG = false;
     WSRPC.TRACE = false;
-       
+
     async function myCallBack(){
         log.warn("Triggered index.jsx");
     }
     // importing through manifest.xml isn't working because relative paths
     // possibly TODO
     jsx.evalFile('./host/index.jsx', myCallBack);
-    
+
     async function runEvalScript(script) {
         // because of asynchronous nature of functions in jsx
         // this waits for response
@@ -26,7 +26,7 @@
             csInterface.evalScript(script, resolve);
         });
     }
-    
+
     /** main entry point **/
     startUp("WEBSOCKET_URL");
 
@@ -34,7 +34,7 @@
     async function startUp(url){
         log.warn("url", url);  
         promis = runEvalScript("getEnv('" + url + "')");
-        
+
         var res = await promis; 
         // run rest only after resolved promise
         main(res);
@@ -42,28 +42,39 @@
 
     async function get_extension_version(){
         /** Returns version number from extension manifest.xml **/
-        log.debug("get_extension_version")
-        var path = csInterface.getSystemPath(SystemPath.EXTENSION);
-        log.debug("extension path " + path);
-    
-        var result = window.cep.fs.readFile(path + "/CSXS/manifest.xml");
-        var version = undefined;
-        if(result.err === 0){
-            if (window.DOMParser) {
-                const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(result.data.toString(), 'text/xml');
-                const children = xmlDoc.children;
-        
-                for (let i = 0; i <= children.length; i++) {
-                    if (children[i] && children[i].getAttribute('ExtensionBundleVersion')) {
-                        version = children[i].getAttribute('ExtensionBundleVersion');
-                    }
+        {
+            log.debug("get_extension_version").then(
+                var path = csInterface.getSystemPath(SystemPath.EXTENSION);
+            ).then(
+                log.debug("extension path " + path);
+            );
+        }.then(
+            {
+                var result = window.cep.fs.readFile(path + "/CSXS/manifest.xml");
+                var version = undefined;
+            }
+        ).then(
+            if(result.err === 0){
+                if (window.DOMParser) {
+                    {
+                        const parser = new DOMParser();
+                        const xmlDoc = parser.parseFromString(result.data.toString(), 'text/xml');
+                        const children = xmlDoc.children;
+                    }.then(
+                        for (let i = 0; i <= children.length; i++) {
+                            if (children[i] && children[i].getAttribute('ExtensionBundleVersion')) {
+                                version = children[i].getAttribute('ExtensionBundleVersion');
+                            }
+                        }
+                    );
                 }
             }
-        }
-        return version
+        ).then(
+            return version;
+        );
+
     }
-             
+
     async function main(websocket_url){
       urlPromise = new Promise(function(resolve) {
           log.warn("websocket_url", websocket_url);
@@ -79,9 +90,9 @@
           result = new WSRPC(urlPromise, 5000);
           resolve(result);
       }));
-  
+
       await RPC.then(RPC.connect().then(log.warn("connected")));
-      
+
       async function EscapeStringForJSX(str){
       // Replaces:
       //  \ with \\
@@ -90,7 +101,7 @@
       // See: https://stackoverflow.com/a/3967927/5285364
           return str.replace(/\\/g, '\\\\').replace(/'/g, "\\'").replace(/"/g, '\\"');
       }
-      
+
       RPC.addRoute('Photoshop.open', async function (data) {
               log.warn('Server called client route "open":', data);
               var escapedPath = EscapeStringForJSX(data.path);
@@ -100,7 +111,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.read', async function (data) {
               log.warn('Server called client route "read":', data);
               return runEvalScript("getHeadline()")
@@ -109,7 +120,7 @@
                       return result;
                   });
       });
-  
+
       RPC.addRoute('Photoshop.get_layers', async function (data) {
               log.warn('Server called client route "get_layers":', data);
               return runEvalScript("getLayers()")
@@ -118,7 +129,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.set_visible', async function (data) {
               log.warn('Server called client route "set_visible":', data);
               return runEvalScript("setVisible(" + data.layer_id + ", " +
@@ -128,7 +139,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.get_active_document_name', async function (data) {
               log.warn('Server called client route "get_active_document_name":', 
                         data);
@@ -138,7 +149,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.get_active_document_full_name', async function (data) {
               log.warn('Server called client route ' +
                        '"get_active_document_full_name":', data);
@@ -148,41 +159,41 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.save', async function (data) {
               log.warn('Server called client route "save":', data);
-              
+
               return runEvalScript("save()")
                   .then(async function(result){
                       log.warn("save: " + result);
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.get_selected_layers', async function (data) {
               log.warn('Server called client route "get_selected_layers":', data);
-              
+
               return runEvalScript("getSelectedLayers()")
                   .then(async function(result){
                       log.warn("get_selected_layers: " + result);
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.create_group', async function (data) {
               log.warn('Server called client route "create_group":', data);
-              
+
               return runEvalScript("createGroup('" + data.name + "')")
                   .then(async function(result){
                       log.warn("createGroup: " + result);
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.group_selected_layers', async function (data) {
               log.warn('Server called client route "group_selected_layers":', 
                        data);
-              
+
               return runEvalScript("groupSelectedLayers(null, "+
                                    "'" + data.name +"')")
                   .then(async function(result){
@@ -190,7 +201,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.import_smart_object', async function (data) {
               log.warn('Server called client "import_smart_object":', data);
               var escapedPath = EscapeStringForJSX(data.path);
@@ -202,7 +213,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.replace_smart_object', async function (data) {
               log.warn('Server called route "replace_smart_object":', data);
               var escapedPath = EscapeStringForJSX(data.path);
@@ -214,7 +225,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.delete_layer', async function (data) {
               log.warn('Server called route "delete_layer":', data);
               return runEvalScript("deleteLayer("+data.layer_id+")")
@@ -233,7 +244,7 @@
                 return result;
             });
 });
-       
+
       RPC.addRoute('Photoshop.select_layers', async function (data) {
               log.warn('Server called client route "select_layers":', data);
               
@@ -243,7 +254,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.is_saved', async function (data) {
               log.warn('Server called client route "is_saved":', data);
               
@@ -253,7 +264,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.saveAs', async function (data) {
               log.warn('Server called client route "saveAsJPEG":', data);
               var escapedPath = EscapeStringForJSX(data.image_path);
@@ -265,7 +276,7 @@
                       return result;
                   });
       });
-      
+
       RPC.addRoute('Photoshop.imprint', async function (data) {
               log.warn('Server called client route "imprint":', data);
               var escaped = data.payload.replace(/\n/g, "\\n");
@@ -285,7 +296,7 @@
         log.warn('Server called client route "close":', data);
         return runEvalScript("close()");
       });
-        
+
       RPC.call('Photoshop.ping').then(async function (data) {
           log.warn('Result for calling server route "ping": ', data);
           return runEvalScript("ping()")
@@ -293,11 +304,11 @@
                       log.warn("ping: " + result);
                       return result;
                   });
-                
+
       }, async function (error) {
           log.warn(error);
       });
-    
+
     }
-    
+
     log.warn("end script");
